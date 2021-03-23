748b3a3668f315add72aff808af256e219e74d76
cnjohnniekwok <30383300+cnjohnniekwok@users.noreply.github.com>
2021-03-23 12:26:44 +0800

    Add files via upload
    added change password page, a working copy

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 
-// Simple login using bycrypt
-//-----------------------------------------------------------------|
-// const bcrypt = require("bcrypt");
-// const saltRounds = 10;
-
+// Initalize app using express
 const app = express();
 
 app.use(express.static("public"));
@@ -62,13 +58,16 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//=================================================================|
+//                      DEMO SITE POST ROUTE
+//-----------------------------------------------------------------|
 app.post("/login", (req, res) => {
   const user = new User({
     username: req.body.username,
     password: req.body.password
   });
 
-  //this functino came from passport.
+  //this function came from passport.
   req.login(user, (err) => {
     if (!err) {
       passport.authenticate("local")(req, res, () => {
@@ -79,49 +78,13 @@ app.post("/login", (req, res) => {
       res.render("login", {
         userAlreadyExisted: "Invalid username or passward.",
         pageTitle: "Login Page",
+        username: req.session.passport.user,
         authorized: req.isAuthenticated()
       });
     }
   });
-  // Simple login using bycrypt
-  //-----------------------------------------------------------------|
-  // userName = req.body.username;
-  // password = req.body.password;
-  //
-  // UserPass.findOne({
-  //   userName: userName
-  // }, (err, resultObject) => {
-  //   if (!err) {
-  //     if (!resultObject) {
-  //       res.render('login', {
-  //         userAlreadyExisted: "Invalid username or password",
-  //         pageTitle: "Login Page"
-  //       });
-  //     } else {
-  //       bcrypt.compare(password, resultObject.password, (err, result) => {
-  //         if (!err) {
-  //           if(result){
-  //             res.render('private', {
-  //               pageTitle: "Private"
-  //             });
-  //           }
-  //           else{
-  //             res.render('login', {
-  //               userAlreadyExisted: "Invalid username or password",
-  //               pageTitle: "Login Page"
-  //             });
-  //           }
-  //         } else {
-  //           console.log("An error found -: " + err);
-  //         }
-  //       });
-  //     }
-  //   } else {
-  //     console.log("An error found -: " + err);
-  //   }
-  // });
-
 });
+
 app.post("/register", (req, res) => {
   // This function came from passport mongoose
   User.register({
@@ -137,63 +100,96 @@ app.post("/register", (req, res) => {
       res.render("register", {
         userAlreadyExisted: "Something went worng, please try again...",
         pageTitle: "Registration",
+        username: req.session.passport.user,
         authorized: req.isAuthenticated()
       });
     }
   });
-
-  // Simple login using bycrypt
-  //-----------------------------------------------------------------|
-  // userName = req.body.username;
-  // password = req.body.password;
-  // UserPass.findOne({
-  //   userName: userName
-  // }, (err, resultObject) => {
-  //   if (!err) {
-  //     if (!resultObject) {
-  //
-  //       bcrypt.hash(password, saltRounds, (err, hash) => {
-  //         if (!err) {
-  //           let userPassDoc = new UserPass({
-  //             userName: req.body.username,
-  //             password: hash
-  //           });
-  //           userPassDoc.save((err) => {
-  //             if (!err) {
-  //               console.log("Registered.");
-  //               res.render('login', {
-  //                 userAlreadyExisted: "",
-  //                 pageTitle: "Login Page"
-  //               });
-  //             } else {
-  //               console.log("An error found -: " + err);
-  //             }
-  //           });
-  //         } else {
-  //           console.log("An error found -: " + err);
-  //         }
-  //       });
-  //
-  //     } else {
-  //       res.render('register', {
-  //         userAlreadyExisted: "Email already used.",
-  //         pageTitle: "Registration"
-  //       });
-  //     }
-  //   } else {
-  //     console.log("An error found -: " + err);
-  //   }
-  // });
 });
 
-// /private route relies on passport to authenticate
+function renderSettingPage(req, res, renderMessage){
+  res.render("settings", {
+    userAlreadyExisted: renderMessage,
+    pageTitle: "Settings",
+    username: req.session.passport.user,
+    authorized: req.isAuthenticated()
+  });
+}
+
+function renderPrivatePage(req ,res){
+  res.render("private", {
+    pageTitle: "Authenticated!",
+    username: req.session.passport.user,
+    authorized: req.isAuthenticated()
+  });
+}
+
+app.post("/settings", (req, res)=>{
+  const currentUser = req.session.passport.user;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+  const confirmPswd = req.body.confirmPswd;
+  if(newPassword === confirmPswd){
+    User.findOne({username:currentUser}, (err, resultObject)=>{
+      if(!err){
+        if(resultObject){
+          resultObject.changePassword(oldPassword, newPassword, (err)=>{
+            if(!err){
+              res.render("login", {
+                userAlreadyExisted: "Password updated, please login again.",
+                pageTitle: "Login Page",
+                username: req.session.passport.user,
+                authorized: req.isAuthenticated()
+              });
+            }
+            else{
+              renderSettingPage(req, res, "Somehting went wrong, please try again...");
+            }
+          });
+        }
+        else{
+          renderSettingPage(req, res, "User does not exist in our record, please signout and try again.");
+        }
+      }
+      else{
+        renderSettingPage(req, res, "Somehting went wrong, please try again...");
+      }
+    });
+  }
+  else{
+    renderSettingPage(req, res, "Both columns of new password must be the same.");
+  }
+
+});
+//=================================================================|
+//                      DEMO SITE GET ROUTE
+//-----------------------------------------------------------------|
+
+app.get("/", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("home", {
+      loggedout: "",
+      pageTitle: "Welcome",
+      username: req.session.passport.user,
+      authorized: req.isAuthenticated()
+    });
+  }
+  else{
+    res.render("home", {
+      loggedout: "",
+      pageTitle: "Welcome",
+      username: "",
+      authorized: req.isAuthenticated()
+    });
+  }
+
+});
+
+// "/private" route relies on passport to authenticate
 //-----------------------------------------------------------------|
 app.get("/private", (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("private", {
-      pageTitle: "Authenticated!",
-      authorized: req.isAuthenticated()
-    });
+    renderPrivatePage(req, res);
   } else {
     res.redirect("/login");
   }
@@ -201,14 +197,12 @@ app.get("/private", (req, res) => {
 
 app.get("/register", (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("private", {
-      pageTitle: "Authenticated!",
-      authorized: req.isAuthenticated()
-    });
+    renderPrivatePage(req, res);
   } else {
     res.render("register", {
       userAlreadyExisted: "",
       pageTitle: "Registration",
+      username: "",
       authorized: req.isAuthenticated()
     });
   }
@@ -216,25 +210,15 @@ app.get("/register", (req, res) => {
 
 app.get("/login", (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("private", {
-      pageTitle: "Authenticated!",
-      authorized: req.isAuthenticated()
-    });
+    renderPrivatePage(req, res);
   } else {
     res.render("login", {
       userAlreadyExisted: "",
       pageTitle: "Login Page",
+      username: "",
       authorized: req.isAuthenticated()
     });
   }
-});
-
-app.get("/", (req, res) => {
-  res.render("home", {
-    loggedout: "",
-    pageTitle: "Welcome",
-    authorized: req.isAuthenticated()
-  });
 });
 
 app.get("/logout", (req, res)=>{
@@ -243,11 +227,19 @@ app.get("/logout", (req, res)=>{
   res.render("home", {
     loggedout: "You have successfully logged out.",
     pageTitle: "Welcome",
+    username: "",
     authorized: req.isAuthenticated()
   });
-
 });
 
-app.listen(process.env.SERVER_LISTEN_PORT, (err) => {
-  console.log("Server is successfully running at prot -: " + process.env.SERVER_LISTEN_PORT);
+app.get("/settings", (req, res) => {
+  if (req.isAuthenticated()) {
+    renderSettingPage(req, res, "");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.listen(process.env.PORT, (err) => {
+  console.log("Server is successfully running at prot -: " + process.env.PORT);
 });
